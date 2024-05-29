4e1a39c454c272d39ce65fd5aade4a5524495c0f
pod153 <kmell@protonmail.com>
2024-05-28 22:58:41 -0500

    Define JSON body for login route

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,14 @@
-use axum::{http::StatusCode, response::IntoResponse};
+use axum::{http::StatusCode, Json, response::IntoResponse};
+use serde::{Deserialize, Serialize};
 
-pub async fn login() -> impl IntoResponse {
+#[derive(Deserialize, Serialize, Debug)]
+pub struct LoginRequest{
+    pub email: String,
+    pub password: String,
+}
+
+pub async fn login(
+    Json(request): Json<LoginRequest>
+) -> impl IntoResponse {
     StatusCode::OK.into_response()
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -1,9 +1,28 @@
+use serde_json::Value;
 use crate::helpers::TestApp;
 
 #[tokio::test]
-async fn login_returns_ok() {
-    let app = TestApp::new().await;
-    let response = app.post_login().await;
+async fn should_return_422_if_malformed_credentials() {
 
-    assert_eq!(response.status().as_u16(), 200);
+    // Given
+    let app: TestApp = TestApp::new().await;
+
+    let test_cases: [Value; 2] = [
+        serde_json::json!({
+            "name": "",
+            "lastname": "********"
+        }),
+        serde_json::json!({
+            "e-mail": "user@gmail.com",
+            "pass-code": "**"
+        }),
+    ];
+
+    for test_body in test_cases.iter() {
+        // When
+        let response = app.post_login(test_body).await;
+        //Then
+        assert_eq!(response.status().as_u16(), 422, "Failed for input {:?}", test_body);
+    }
+
 }
