afaa9f1e4eb12baeb9654af0ff89161f1856d12d
pod153 <kmell@protonmail.com>
2024-05-28 23:56:11 -0500

    Define login route

--- auth-service/src/routes/login.rs
+++ auth-service/src/routes/login.rs
@@ -1,5 +1,9 @@
 use axum::{http::StatusCode, Json, response::IntoResponse};
+use axum::extract::State;
 use serde::{Deserialize, Serialize};
+use crate::app_state::AppState;
+use crate::domain::{AuthAPIError, Email, Password};
+use crate::routes::SignupResponse;
 
 #[derive(Deserialize, Serialize, Debug)]
 pub struct LoginRequest{
@@ -7,8 +11,33 @@ pub struct LoginRequest{
     pub password: String,
 }
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct LoginResponse {
+    pub message: String,
+}
+
 pub async fn login(
+    State(state): State<AppState>,
     Json(request): Json<LoginRequest>
-) -> impl IntoResponse {
-    StatusCode::OK.into_response()
+) -> Result<impl IntoResponse, AuthAPIError> {
+
+    // Validations
+    let email = Email::parse(request.email.clone())
+        .map_err(|_| AuthAPIError::InvalidCredentials)?;
+    let password = Password::parse(request.password.clone())
+        .map_err(|_| AuthAPIError::InvalidCredentials)?;
+
+    let user_store = &state.user_store.read().await;
+
+    if user_store.validate_user(&email, &password).await.is_err() {
+        return Err(AuthAPIError::IncorrectCredentials);
+    }
+    let user = user_store.get_user(&email).await
+        .map_err(|_| AuthAPIError::IncorrectCredentials)?;
+
+    let response = Json(LoginResponse {
+        message: "Login successful".to_string()
+    });
+    Ok((StatusCode::OK, response))
+
 }
--- auth-service/tests/api/login.rs
+++ auth-service/tests/api/login.rs
@@ -26,3 +26,51 @@ async fn should_return_422_if_malformed_credentials() {
     }
 
 }
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+
+    // Given
+    let app: TestApp = TestApp::new().await;
+
+    let test_cases: [Value; 3] = [
+        serde_json::json!({
+            "email": "",
+            "password": "********"
+        }),
+        serde_json::json!({
+            "email": "gmail.com",
+            "password": "********"
+        }),
+        serde_json::json!({
+            "email": "use@gmail.com",
+            "password": "123"
+        }),
+    ];
+
+    for test_body in test_cases.iter() {
+        // When
+        let response = app.post_login(test_body).await;
+        //Then
+        assert_eq!(response.status().as_u16(), 400, "Failed for input {:?}", test_body);
+    }
+
+}
+
+#[tokio::test]
+async fn should_return_401_if_incorrect_credentials() {
+
+    // Given
+    let app: TestApp = TestApp::new().await;
+    let body = serde_json::json!(
+        {
+            "email": "user@gmail.com",
+            "password": "87654321"
+        }
+    );
+
+    // When-Then
+    let response = app.post_login(&body).await;
+    assert_eq!(response.status().as_u16(), 401, "Failed for input {:?}", body);
+
+}
