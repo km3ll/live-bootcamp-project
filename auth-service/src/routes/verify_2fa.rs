77f13912a2fff9bde978c17632c7fd4d7275eab6
km3ll <kmell@protonmail.com>
2024-06-10 19:38:53 -0500

    Complete task 4 - sub-tasks 4 and 5

--- auth-service/src/routes/verify_2fa.rs
+++ auth-service/src/routes/verify_2fa.rs
@@ -1,6 +1,10 @@
 use axum::{http::StatusCode, response::IntoResponse, Json};
 use serde::Deserialize;
 
+use crate::{
+    domain::{AuthAPIError, Email, LoginAttemptId, TwoFACode},
+};
+
 #[derive(Debug, Deserialize)]
 pub struct Verify2FARequest {
     pub email: String,
@@ -10,6 +14,25 @@ pub struct Verify2FARequest {
     pub two_fa_code: String,
 }
 
-pub async fn verify_2fa(Json(request): Json<Verify2FARequest>) -> impl IntoResponse {
-    StatusCode::OK.into_response()
+pub async fn verify_2fa(
+    Json(request): Json<Verify2FARequest>,
+) -> Result<impl IntoResponse, AuthAPIError> {
+    
+    let email = match Email::parse(request.email.clone()) {
+        Ok(email) => email,
+        Err(_) => return Err(AuthAPIError::InvalidCredentials)
+    };
+
+    let login_attempt_id = match LoginAttemptId::parse(request.login_attempt_id.clone()) {
+        Ok(login_attempt_id) => login_attempt_id,
+        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+    };
+
+    let two_fa_code = match TwoFACode::parse(request.two_fa_code) {
+        Ok(two_fa_code) => two_fa_code,
+        Err(_) => return Err(AuthAPIError::InvalidCredentials),
+    };
+    
+    Ok(StatusCode::OK.into_response())
+
 }
--- auth-service/tests/api/verify_2fa.rs
+++ auth-service/tests/api/verify_2fa.rs
@@ -1,6 +1,63 @@
-use auth_service::domain::LoginAttemptId;
+use auth_service::{
+    domain::{LoginAttemptId, TwoFACode},
+    ErrorResponse,
+};
 
 use crate::helpers::{get_random_email, TestApp};
+
+#[tokio::test]
+async fn should_return_400_if_invalid_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+    let login_attempt_id = LoginAttemptId::default().as_ref().to_owned();
+    let two_fa_code = TwoFACode::default().as_ref().to_owned();
+
+    let test_cases = vec![
+        (
+            "invalid_email",
+            login_attempt_id.as_str(),
+            two_fa_code.as_str(),
+        ),
+        (
+            random_email.as_str(),
+            "invalid_login_attempt_id",
+            two_fa_code.as_str(),
+        ),
+        (
+            random_email.as_str(),
+            login_attempt_id.as_str(),
+            "invalid_two_fa_code",
+        ),
+        ("", "", ""),
+    ];
+
+    for (email, login_attempt_id, code) in test_cases {
+        let request_body = serde_json::json!({
+            "email": email,
+            "loginAttemptId": login_attempt_id,
+            "2FACode": code
+        });
+
+        let response = app.post_verify_2fa(&request_body).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            400,
+            "Failed for input: {:?}",
+            request_body
+        );
+
+        assert_eq!(
+            response
+                .json::<ErrorResponse>()
+                .await
+                .expect("Could not deserialize response body to ErrorResponse")
+                .error,
+            "Invalid credentials".to_owned()
+        );
+    }
+}
 
 #[tokio::test]
 async fn should_return_422_if_malformed_input() {
