57909db3d0799df0526e5d739c083453a837b7a5
km3ll <kmell@protonmail.com>
2024-06-10 19:11:56 -0500

    Complete task 4 - sub-tasks 1 and 2

--- auth-service/tests/api/verify_2fa.rs
+++ auth-service/tests/api/verify_2fa.rs
@@ -1,8 +1,47 @@
-use crate::helpers::TestApp;
+use auth_service::domain::LoginAttemptId;
+
+use crate::helpers::{get_random_email, TestApp};
 
 #[tokio::test]
-async fn verify_2fa_returns_ok() {
+async fn should_return_422_if_malformed_input() {
     let app = TestApp::new().await;
-    let response = app.post_verify_2fa().await;
-    assert_eq!(response.status().as_u16(), 200);
+
+    let random_email = get_random_email();
+    let login_attempt_id = LoginAttemptId::default().as_ref().to_owned();
+
+    let test_cases = [
+        serde_json::json!({
+            "2FACode": "123456",
+        }),
+        serde_json::json!({
+            "email": random_email,
+        }),
+        serde_json::json!({
+            "loginAttemptId": login_attempt_id,
+        }),
+        serde_json::json!({
+            "2FACode": "123456",
+            "email": random_email,
+        }),
+        serde_json::json!({
+            "2FACode": "123456",
+            "loginAttemptId": login_attempt_id,
+        }),
+        serde_json::json!({
+            "email": random_email,
+            "loginAttemptId": login_attempt_id,
+        }),
+        serde_json::json!({}),
+    ];
+
+    for test_case in test_cases {
+        let response = app.post_verify_2fa(&test_case).await;
+
+        assert_eq!(
+            response.status().as_u16(),
+            422,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
 }
